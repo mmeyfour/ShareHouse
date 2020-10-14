6451853b3dc5fd4d948efaa38f6f991ab49218e8
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-14 13:29:06 +0200

    no funca

--- ShareHouse/ViewController.swift
+++ ShareHouse/ViewController.swift
@@ -8,44 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    loadSampleRentalDetail()
-    loadMalagaRentalList()
-    // Do any additional setup after loading the view.
-  }
-  func loadSampleRentalDetail() {
-    guard let url = Bundle.main.url(forResource: "RentalDetail", withExtension: "json"),
-      let data = try? Data(contentsOf: url) else {
-        return
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSampleRentalDetail()
+        loadMalagaRentalList()
+        // Do any additional setup after loading the view.
     }
-    let dateFormatterGet = DateFormatter()
-    dateFormatterGet.dateFormat = "yyyy-MM-dd"
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-    decoder.dateDecodingStrategy = .formatted(dateFormatterGet)
-    guard let rentalDetail = try? decoder.decode(RentalDetail.self, from: data) else {
-      return
+    func loadSampleRentalDetail() {
+        guard let url = Bundle.main.url(forResource: "RentalDetail", withExtension: "json"),
+              let data = try? Data(contentsOf: url) else {
+            return
+        }
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .formatted(dateFormatterGet)
+        guard let rentalDetail = try? decoder.decode(RentalDetail.self, from: data) else {
+            return
+        }
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd MMMM yyyy"
+        print("El propietario \(rentalDetail.realtor) a fecha de \(dateFormatterPrint.string(from: rentalDetail.dateAdded)) alquila el apartamento en \(rentalDetail.name) con \(rentalDetail.floorArea) m^2 al precio de \(rentalDetail.monthlyPrice) €/mes")
     }
-    let dateFormatterPrint = DateFormatter()
-    dateFormatterPrint.dateFormat = "dd MMMM yyyy"
-    print("El propietario \(rentalDetail.realtor) a fecha de \(dateFormatterPrint.string(from: rentalDetail.dateAdded)) alquila el apartamento en \(rentalDetail.name) con \(rentalDetail.floorArea) m^2 al precio de \(rentalDetail.monthlyPrice) €/mes")
-  }
-  func loadMalagaRentalList() {
-    guard let url = Bundle.main.url(forResource: "MalagaRentalList", withExtension: "json"),
-       let data = try? Data(contentsOf: url) else {
-      return
+    
+    func loadMalagaRentalList() {
+        guard let url = Bundle.main.url(forResource: "MalagaRentalList", withExtension: "json"),
+              let data = try? Data(contentsOf: url) else {
+            return
+        }
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .formatted(dateFormatterGet)
+        let malagaRentalList = try! decoder.decode([String:RentalDetail].self, from: data)
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd MMMM yyyy"
+        for (key,value) in malagaRentalList {
+            print("Esta es la calle \(key) con \(value)")
+        }
     }
-    let dateFormatterGet = DateFormatter()
-    dateFormatterGet.dateFormat = "yyyy-MM-dd"
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-    decoder.dateDecodingStrategy = .formatted(dateFormatterGet)
-    let malagaRentalList = try! decoder.decode([String:RentalDetail].self, from: data)
-    let dateFormatterPrint = DateFormatter()
-    dateFormatterPrint.dateFormat = "dd MMMM yyyy"
-    for (key,value) in malagaRentalList {
-      print("Esta es la calle \(key) con \(value)")
-    }
-  }
 }
