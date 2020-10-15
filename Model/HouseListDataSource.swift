15cd14a9350f360332d448d73500f83bdbc6ac67
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-15 10:21:17 +0200

    HouseListDataSource Modificated !

--- Model/HouseListDataSource.swift
+++ Model/HouseListDataSource.swift
@@ -30,58 +30,12 @@ class HouseListDataSource: NSObject {
         let malagaRentalList = try! decoder.decode([String:HouseDetail].self, from: data)
         let dateFormatterPrint = DateFormatter()
         dateFormatterPrint.dateFormat = "dd MMMM yyyy"
-        //        for (key,value) in malagaRentalList {
-        //           print("Esta es la calle \(key) con \(value)")
-        //            print(key)
-        //            print(value)
-        //        }
-        //        print(malagaRentalList)
-        for (key, value) in malagaRentalList {
-            print("\(key): \(value)")
-        }
+//        for (key, value) in malagaRentalList {
+//            print("\(key): \(value)")
+//        }
         let houses = malagaRentalList.map {(key, house)  in
-            HouseSummaryViewModel(image: UIImage.mainImage(houseId: "1"), name: house.name, realtor: house.realtor, monthlyPrice: house.monthlyPrice, rooms: house.rooms, floorArea: house.floorArea, description: house.description, dateAdded: house.dateAdded)
+            HouseSummaryViewModel(image: UIImage.mainImage(houseId: "1"), name: house.name, realtor: house.realtor, monthlyPrice: house.monthlyPrice, rooms: house.rooms, floorArea: house.floorArea, description: house.description, dateAdded: house.dateAdded, location: house.location)
         }
         return houses
     }
-}
-func loadMalagaRentalList() {
-    guard let url = Bundle.main.url(forResource: "HouseList", withExtension: "json"),
-          let data = try? Data(contentsOf: url) else {
-        return
-    }
-    let dateFormatterGet = DateFormatter()
-    dateFormatterGet.dateFormat = "yyyy-MM-dd"
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-    decoder.dateDecodingStrategy = .formatted(dateFormatterGet)
-    let malagaRentalList = try! decoder.decode([String:HouseDetail].self, from: data)
-    let dateFormatterPrint = DateFormatter()
-    dateFormatterPrint.dateFormat = "dd MMMM yyyy"
-    for (key,value) in malagaRentalList {
-        //            print("Esta es la calle \(key) con \(value)")
-        print(key)
-        print(value)
-    }
-}
-
-extension HouseListDataSource: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return houses.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //        1. Dequeue cell
-        guard let houseCell = tableView.dequeueReusableCell(withIdentifier: HouseTableViewCell.identifier, for: indexPath) as? HouseTableViewCell else {
-            fatalError("Wrong type of cell")
-        }
-        
-        //        2. Prepare cell
-        let currentHouse = houses[indexPath.row]
-        houseCell.configure(houseSummary: currentHouse)
-        
-        //        3. Return configured cell
-        return houseCell
-    }
-    
 }
