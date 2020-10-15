3dbcd4d95c329c9c2ce61e444779f9b240d898a5
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-15 10:23:10 +0200

    HouseListDataSource extention created !

--- Model/HouseListDataSource.swift
+++ Model/HouseListDataSource.swift
@@ -39,3 +39,24 @@ class HouseListDataSource: NSObject {
         return houses
     }
 }
+
+extension HouseListDataSource: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return houses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //        1. Dequeue cell
+        guard let houseCell = tableView.dequeueReusableCell(withIdentifier: HouseTableViewCell.identifier, for: indexPath) as? HouseTableViewCell else {
+            fatalError("Wrong type of cell")
+        }
+        
+        //        2. Prepare cell
+        let currentHouse = houses[indexPath.row]
+        houseCell.configure(houseSummary: currentHouse)
+        
+        //        3. Return configured cell
+        return houseCell
+    }
+    
+}
