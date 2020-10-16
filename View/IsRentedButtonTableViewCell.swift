13df1b791b7627f4aa84da6a89067b276f41a77b
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-16 10:41:54 +0200

    buttoncell created

---
+++ View/IsRentedButtonTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IsRentedButtonTableViewCell.swift
+//  ShareHouse
+//
+//  Created by Mersad Meyfour Asadi on 16/10/2020.
+//
+
+import UIKit
+
+class IsRentedButtonTableViewCell: UITableViewCell {
+
+    static let identifier = "IsRentedButtonTableViewCell"
+    
+}
