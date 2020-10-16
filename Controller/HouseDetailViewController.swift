4a740e9ea77ca1e37dbed8ea51dea121fad66d89
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-16 08:54:07 +0200

    HouseDetailTableView created !

---
+++ Controller/HouseDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HouseDetailViewController.swift
+//  ShareHouse
+//
+//  Created by Mersad Meyfour Asadi on 16/10/2020.
+//
+
+import UIKit
+
+class HouseDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+//    let houseDetailDataSource = HouseDetailDataSource()
+    static let segueIdentifier = "ShowHouseDetail"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
